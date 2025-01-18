39af6085e7e10708e9e1234d7fb678549642b3cb
WestMinsu <55283371+WestMinsu@users.noreply.github.com>
2025-01-19 01:07:14 +0900

    test

--- config.js
+++ config.js
@@ -13,10 +13,10 @@ const siteConfig = {
 const users = [
   {
     id: 0, // default author
-    username: "minsu",
+    username: "licat",
     company: "weniv",
     position: "CEO",
-    img: "img/user/profile-mura.png",
+    img: "img/user/profile-licat.png",
   },
 ];
 
